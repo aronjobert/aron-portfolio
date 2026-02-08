79294a3b8e9c6bb6b1182305754028a824e58ff0
aronjobert <aronjobertsalim@gmail.com>
2026-02-08 17:34:10 +0800

    contact form api updated

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   const response = await resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
-    to: ['aronjobertsalim@gmail.com'],
+    to: ['delivered@resend.dev'],
     subject: 'hello world',
     html: '<strong>it works!</strong>',
   });
